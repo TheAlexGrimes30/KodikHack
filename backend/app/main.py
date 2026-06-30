debeee10ffdb90ef46ced1b827a883387cffd840
alexborodach30 <sagopien04@gmail.com>
2026-06-30 19:31:57 +0300

    Main app init.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from backend.db.database import engine
 
 
@@ -11,3 +11,31 @@ async def lifespan(app: FastAPI):
 
     yield
     await engine.dispose()
+
+app = FastAPI( title="BussinesAI",
+               description="AI assistant for business ideas",
+               version="1.0.0",
+               lifespan=lifespan
+             )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    """Return the application status message."""
+
+    return {"message": "Backend is running"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
